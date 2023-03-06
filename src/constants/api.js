2d35aed0694d08134ce3eb27412028ecaf5e2300
Jennygra <83270722+Jennygra@users.noreply.github.com>
2023-03-06 11:15:42 +0100

    Update api.js

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://jennygramdal.com/random-api/wp-json/";
+export const BASE_URL = "https://jennygramdal.com/random-api/wp-json/";
 export const PRODUCT_PATH = "wc/store/products";
 export const TOKEN_PATH = "jwt-auth/v1/token";
